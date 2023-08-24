43559e19bb99736187bff68ccee044f5b59d32e0
dymjin <dymjin@gmail.com>
2023-08-24 12:11:16 +0200

    Modify calcBtns + change styles* *calcBtns are now divs, change color on hover and on click, add border below display

--- JS.js
+++ JS.js
@@ -12,7 +12,7 @@ let calculator = {
     "*": (a, b) => a * b,
     "/": (a, b) => a / b,
     calculate(a, b, op) {
-        if (typeof (a) == typeof (0) && typeof (b) == typeof (0) && `${op}` in calculator) {
+        if (typeof (a) == "number" && typeof (b) == "number" && `${op}` in calculator) {
             return `${op}` in calculator == true ? calculator[op](+a, +b) : "";
         } else {
             console.log("ERROR: Invalid operation");
@@ -24,18 +24,49 @@ let calculator = {
 /*
  if (user clicks calculate btn) {
     displayValue = calcDisplay;
-    take displayValue and split into 3 parts:
-        1. first number(any length, can be negative, can be decimal)
-        2. operator(+,-,*,/,etc.)
-        3. second number(same stuff as first num)
     store calculate(first num, second num, operator) in sum
     update calcDisplay with sum
 }
+
+while (user inputs first num) {
+    if (user input is any operator(+/-*)) {
+        split displayValue and slice off end value,
+        store sliced off value as operator
+        store rest of nums.join("") as first num
+        if (second num == undefined) {
+            continue
+        } else if (typeof(+second num) == typeof(0)) {
+            calculator.calculate(first num, second num, operator)
+            first num = null;
+            second num = nul;
+            operator = null;
+        }
+    }
+}
+
 */
 
+// for (let key in calculator) {
+//     if (calcDisplay.textContent.split("").includes(key)) {
+//         parts = calcDisplay.textContent.split("");
+//         op = parts.slice(parts.length - 1);
+//         console.log(op);
+//         console.log(parts)
+//     }
+// }
+
+// while (typeof (+calcDisplay.textContent) == "number") {
+
+
+//     continue;
+// }
+
+// for (let key in calculator) {
+//     console.log(key)
+// }
 calculateBtn.addEventListener('click', () => {
     displayValue = calcDisplay.textContent;
-    console.log(displayValue)
+
     sum = calculator.calculate(displayValue);
     calcDisplay.textContent = sum;
 });
@@ -47,6 +78,7 @@ inputBtns.forEach(btn => btn.addEventListener('click', () => {
 }));
 calcBtns.forEach(btn => btn.addEventListener('click', () => {
     calcDisplay.textContent += btn.textContent;
+    btn.blur();
 }));
 
 window.addEventListener('keypress', (e) => {
@@ -68,6 +100,6 @@ window.addEventListener('keypress', (e) => {
     } else if (allowedKeys.includes(+e.key) && +e.key !== NaN) {
         calcDisplay.textContent += +(e.key);
     } else if (allowedKeys.includes(e.key)) {
-        calcDisplay.textContent += " " + e.key + " ";
+        calcDisplay.textContent += e.key;
     }
 });
