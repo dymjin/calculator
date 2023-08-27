c154885f19696388ff19cb85f998ce8f524c324d
dymjin <dymjin@gmail.com>
2023-08-27 11:50:39 +0200

    Add numpad support + clr & delete keys

--- JS.js
+++ JS.js
@@ -70,9 +70,22 @@ calcBtns.forEach(btn => btn.addEventListener('click', () => {
 
 window.addEventListener('keypress', (e) => {
     if (e.key == "Enter") {
-        displayValue = calcDisplay.textContent;
-        sum = calculator.calculate(displayValue);
+        // displayValue = calcDisplay.textContent;
+        // sum = calculator.calculate(displayValue);
+        // calcDisplay.textContent = sum;
+        if (num1 == "" || op == null || num2 == "") {
+            num1 = "";
+            num2 = "";
+            op = null;
+            calcDisplay.textContent = "";
+            console.log('ERROR:one or more fields are empty');
+        }
+        sum = calculator.calculate(num1, num2, op);
         calcDisplay.textContent = sum;
+        ans = `${sum}`
+        num1 = "";
+        num2 = "";
+        op = null;
     } else if (e.key == "Delete") {
         calcDisplay.textContent = "";
         num1 = "";
@@ -87,10 +100,24 @@ window.addEventListener('keypress', (e) => {
             calcDisplay.textContent = calcDisplayArr.join("");
         }
     } else if (allowedKeys.includes(+e.key) && +e.key !== NaN) {
-        calcDisplay.textContent += +(e.key);
-        num1 += e.key;
+        calcDisplay.textContent += e.key;
+        if (op == null) {
+            num1 += e.key;
+        } else if (op !== null) {
+            num2 += calcDisplay.textContent.split("").pop();
+        }
     } else if (allowedKeys.includes(e.key)) {
         calcDisplay.textContent += e.key;
-
+        opList = calcDisplay.textContent
+            .replaceAll(/[^-+*\/]/g, "")
+            .split("");
+        if (opList.length > 1 && num1 !== "" && num2 !== "") {
+            calcDisplay.textContent = calculator.calculate(num1, num2, op) + e.key;
+            num1 = calcDisplay.textContent.replaceAll(/[^0-9]/g, "");
+            num2 = "";
+            op = e.key;
+        } else if (typeof (+num1) == "number") {
+            op = calcDisplay.textContent.slice(num1.length)[0];
+        }
     }
 });
